df57c2e41a764de683e597f0e81014a2c75a6c1c
dhruv <dhruvmehra23@gmail.com>
2025-11-08 17:30:56 +0530

    Added hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,22 @@
+import { useRef, useEffect } from "react";
+
+const Hero = () => {
+  const videoRef = useRef();
+
+  useEffect(() => {
+    if (videoRef.current) videoRef.current.playbackRate = 2;
+  }, []);
+  return (
+    <section id="hero">
+      <h1>Macbook Pro</h1>
+      <img src="/title.png" alt="Macbook Title" />
+
+      <video ref={videoRef} src="/videos/hero.mp4" autoPlay muted playsInLine />
+
+      <button>Buy</button>
+      <p>From $1599 or $133/mo for 12 months</p>
+    </section>
+  );
+};
+
+export default Hero;
