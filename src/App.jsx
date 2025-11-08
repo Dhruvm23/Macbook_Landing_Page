98815558295b1a7377c6991d598fd0d55de7d612
dhruv <dhruvmehra23@gmail.com>
2025-11-08 21:11:05 +0530

    Added model switcher

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
-
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+import ProductViewer from "./components/ProductViewer";
+gsap.registerPlugin(ScrollTrigger);
 const App = () => {
   return (
     <div>
       <NavBar />
       <Hero />
+      <ProductViewer />
     </div>
   );
 };
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,11 +8,11 @@ const Hero = () => {
   }, []);
   return (
     <section id="hero">
-      <h1>Macbook Pro</h1>
-      <img src="/title.png" alt="Macbook Title" />
-
+      <div>
+        <h1>Macbook Pro</h1>
+        <img src="/title.png" alt="Macbook Title" />
+      </div>
       <video ref={videoRef} src="/videos/hero.mp4" autoPlay muted playsInLine />
-
       <button>Buy</button>
       <p>From $1599 or $133/mo for 12 months</p>
     </section>
